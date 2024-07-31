5cc115dca5eca3cf334eb2ae65b7b96486b14c91
Zoltan Arvai <zarvai@drag2web.com>
2024-07-31 13:56:51 +0200

    Better psql result sting and safer line procesing

--- mattermost-retention.sh
+++ mattermost-retention.sh
@@ -20,7 +20,7 @@ DB_HOST="127.0.0.1"
 RETENTION="30"
 
 # Mattermost data directory
-DATA_PATH="/mattermost/data/"
+DATA_PATH="/var/opt/mattermost/data/"
 
 # Database drive (postgres OR mysql)
 DB_DRIVE="postgres"
@@ -48,14 +48,14 @@ case $DB_DRIVE in
         ###
         # get list of files to be removed
         ###
-        $DB_DOCKER_CMD psql -h "$DB_HOST" -U"$DB_USER" "$DB_NAME" -t -c "select path from fileinfo where createat < $delete_before;" > /tmp/mattermost-paths.list
-        $DB_DOCKER_CMD psql -h "$DB_HOST" -U"$DB_USER" "$DB_NAME" -t -c "select thumbnailpath from fileinfo where createat < $delete_before;" >> /tmp/mattermost-paths.list
-        $DB_DOCKER_CMD psql -h "$DB_HOST" -U"$DB_USER" "$DB_NAME" -t -c "select previewpath from fileinfo where createat < $delete_before;" >> /tmp/mattermost-paths.list
+        $DB_DOCKER_CMD psql -h "$DB_HOST" -U"$DB_USER" "$DB_NAME" -t --pset=pager=off --no-align -c "select path from fileinfo where path <> '' and createat < $delete_before;" > /tmp/mattermost-paths.list
+        $DB_DOCKER_CMD psql -h "$DB_HOST" -U"$DB_USER" "$DB_NAME" -t --pset=pager=off --no-align -c "select thumbnailpath from fileinfo where thumbnailpath <> '' and createat < $delete_before;" >> /tmp/mattermost-paths.list
+        $DB_DOCKER_CMD psql -h "$DB_HOST" -U"$DB_USER" "$DB_NAME" -t --pset=pager=off --no-align -c "select previewpath from fileinfo where previewpath <> '' createat < $delete_before;" >> /tmp/mattermost-paths.list
 
         ###
         # cleanup db
         ###
-        $DB_DOCKER_CMD psql -h "$DB_HOST" -U"$DB_USER" "$DB_NAME" -t -c "delete from posts where createat < $delete_before;"
+        # $DB_DOCKER_CMD psql -h "$DB_HOST" -U"$DB_USER" "$DB_NAME" -t -c "delete from posts where createat < $delete_before;"
         $DB_DOCKER_CMD psql -h "$DB_HOST" -U"$DB_USER" "$DB_NAME" -t -c "delete from fileinfo where createat < $delete_before;"
     ;;
 
@@ -72,7 +72,7 @@ case $DB_DRIVE in
         ###
         # cleanup db
         ###
-        $DB_DOCKER_CMD mysql --password=$DB_PASS --user=$DB_USER --host=$DB_HOST --database=$DB_NAME --execute="delete from Posts where createat < $delete_before;"
+        # $DB_DOCKER_CMD mysql --password=$DB_PASS --user=$DB_USER --host=$DB_HOST --database=$DB_NAME --execute="delete from Posts where createat < $delete_before;"
         $DB_DOCKER_CMD mysql --password=$DB_PASS --user=$DB_USER --host=$DB_HOST --database=$DB_NAME --execute="delete from FileInfo where createat < $delete_before;"
     ;;
   *)
@@ -85,22 +85,22 @@ esac
 ###
 # delete files
 ###
-for fp in `cat /tmp/mattermost-paths.list`
+while IFS= read -r fp
 do
         if [ -n "$fp" ]; then
                 echo "$DATA_PATH""$fp"
                 $MM_DOCKER_CMD shred -u "$DATA_PATH""$fp"
         fi
-done
-
+done < /tmp/mattermost-paths.list
 
 ###
 # cleanup after script execution
 ###
-rm /tmp/mattermost-paths.list
+NOW=$(date +"%Y-%m-%d-%H-%M")
+mv /tmp/mattermost-paths.list /tmp/mattermost-paths.list--$NOW
 
 ###
 # cleanup empty data dirs
 ###
-$MM_DOCKER_CMD find $DATA_PATH -type d -empty -delete
+# $MM_DOCKER_CMD find $DATA_PATH -type d -empty -delete
 exit 0
